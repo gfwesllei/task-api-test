a4892d1d2446c00f4ee882d309c20d6cedf34b22
wesllei.ferreira <wesllei.gustavo@gmail.com>
2021-05-21 14:42:39 -0300

    Add remove task api

--- src/test/java/br/com/devwell/tasks/ApiTest.java
+++ src/test/java/br/com/devwell/tasks/ApiTest.java
@@ -1,7 +1,9 @@
 package br.com.devwell.tasks;
 
+import groovy.util.logging.Log;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ResponseBody;
 import org.hamcrest.CoreMatchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -40,6 +42,26 @@ public class ApiTest {
                 .body("{\"task\":\"new Task\",\"dueDate\":\"2019-01-01\"}")
                 .when()
                 .post("/todo").then().statusCode(400).body("message", CoreMatchers.is("Due date must not be in past"));
+    }
+
+    @Test
+    public void shouldNotDeleteTask(){
+        Long idDelete =0L;
+        given()
+                .pathParam("id",idDelete)
+                .when()
+                .post("/todo/{id}").then().log().all().statusCode(400).body("message", CoreMatchers.containsString("Task not found"));
+    }
+
+    @Test
+    public void shouldRemoveTask(){
+        Integer idResponse = given()
+                .contentType(ContentType.JSON)
+                .body("{\"task\":\"new Task Remove\",\"dueDate\":\"2030-01-01\"}")
+                .when()
+                .post("/todo").then().statusCode(201).extract().response().path("id");
+
+        given().when().delete("/todo/"+idResponse).then().statusCode(204);
     }
 
 }
